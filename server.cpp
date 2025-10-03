39898064c680dc2ef84ca997627d81d7a09ed825
AnujJha88 <anujjha8788@gmail.com>
2025-10-04 00:32:30 +0530

    server init

--- server.cpp
+++ server.cpp
@@ -1,1 +1,36 @@
 #include "server.h"
+
+ChatServer::ChatServer(int port){
+    //first we want to create the socket
+    server_=socket(AF_INET,SOCK_STREAM,0);//protocol 0 means automatically chosen.AF_INET means we use IPv4 and SOCK_STREAM is TCP 
+
+    if(server_<0){
+        throw std::runtime_error("Socket creation failed");
+    }
+    int option=1;
+    setsockopt(server_,SOL_SOCKET,SO_REUSEADDR,&option,sizeof(option));//set socket option to reuse address 
+    //restart server quickly after crash
+
+    sockaddr_in server_addr{};// this is a struct containing the address family, port number and IP address
+
+
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_addr.s_addr=INADDR_ANY;//listen to all interfaces,makes it 0.0.0.0 as cherno pointed out
+    //might see if there's any improvements we can do here later
+
+    server_addr.sin_port=htons(port);//htons is host to network short and converts port number to network byte order 
+
+    if(bind(server_,(sockaddr*)&server_addr,sizeof(server_addr))<0){
+        throw std::runtime_error("Binding failed");//fail means the port is already in use elsewhere
+    }
+
+    if(listen(server_,5)<0){//listen for incoming connections,5 is the max number of queued connections
+        throw std::runtime_error("Listening failed");
+    }
+
+    channels_["general"]={};//create a default channel called general
+    channels_["cats"]={};
+
+    std::println("Server started on port{} ",port);
+
+}
--- server.h
+++ server.h
@@ -1,4 +1,27 @@
 #include<iostream>
 #include<thread>
 #include<map>
+#include<mutex>
+#include<string>
+#include<set>
+#include<vector>
+#include<print>
+
+
+#include<arpa/inet.h>
+#include<sys/socket.h>
 #include<unistd.h>
+#include<netinet/in.h>
+
+
+class ChatServer{
+    private:
+        int server_;
+        std::mutex client_mutex_;
+        std::map<int,std::string> clients_;//socket->user
+        std::map<std::string, std::set<int>> channels_;//channel->socket
+    
+    public:
+        ChatServer(int port=8888);
+
+    };
